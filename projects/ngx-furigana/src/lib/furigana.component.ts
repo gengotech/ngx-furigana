915d309c340e0b3ebc2eca14b731e5f849ba6b68
gengotech <gengotech@protonmail.com>
2023-09-11 15:07:02 +0200

    Added romaji transliteration

--- projects/ngx-furigana/src/lib/furigana.component.ts
+++ projects/ngx-furigana/src/lib/furigana.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core'
 import {ReadingPair} from "./reading-pair.interface";
 import {FuriganaService} from "./furigana.service";
 import {TransliterateService} from "./transliterate.service";
+import * as wanakana from 'wanakana';
 
 @Component({
   selector: 'furigana',
@@ -13,6 +14,10 @@ export class FuriganaComponent implements OnInit, OnChanges {
   @Input() word!: string;
   @Input() reading!: string | undefined;
   @Input() showFurigana: boolean = true;
+  @Input() forceRomaji: boolean = false;
+
+  originalReading: string | null = null;
+  romajiReading: string | null = null;
 
   transliteredWord: string | null = null;
 
@@ -22,6 +27,18 @@ export class FuriganaComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     if (this.reading) {
+      this.romajiReading = wanakana.toRomaji(this.reading);
+
+      if (!this.originalReading) {
+        this.originalReading = this.reading;
+      }
+
+      if(this.forceRomaji) {
+        this.reading = this.romajiReading;
+      } else {
+        this.reading = this.originalReading;
+      }
+
       this.transliteredWord = this.transliterateService.convertHiraganaInAtoKatakana(this.reading, this.word);
       this.readingPairs = this.furiganaService.getReadingPairs(this.transliteredWord, this.word);
     }
--- projects/ngx-furigana/src/lib/furigana.service.ts
+++ projects/ngx-furigana/src/lib/furigana.service.ts
@@ -120,7 +120,7 @@ export class FuriganaService {
 
     // We remove the readings on specific characters like "ー"
     const readingPairsWithoutSpecificReadings = builtReadingPairs.map(pair => {
-      if (pair.word === "ー" || pair.word === "〜" || pair.word === "～") {
+      if (pair.word === "ー" || pair.word === "〜" || pair.word === "～" || pair.word == "ｰ") {
         pair.reading = null;
       }
       return pair;
